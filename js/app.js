9153868ce95160696c89ed6d1593c52cf9e41c1a
AidzPrehot <aprehatny@yahoo.com>
2018-02-04 14:22:15 -0800

    Added player class and defined render,update, handle input and placed first bug in all enemies array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 // Enemies our player must avoid
+
+let bugMovement = [60, 140, 220]
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 88;
+    this.y = 220;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,23 +19,53 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x++
+    if (this.x === 515) {
+      this.x = -125;
+    }
 };
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+let buggy1 = new Enemy();
+
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+  constructor() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 88;
+    this.y = 220;
+  }
+  update() {
+
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput() {}
+}
+
+let player = new Player() ;
+
+function start(xCord, yCord) {
+player.x = xCord;
+player.y = yCord;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+let allEnemies = [buggy1];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
