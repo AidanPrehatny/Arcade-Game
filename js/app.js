a6122c784de97ff4f5037b1348cf13fd95ceb20a
AidzPrehot <aprehatny@yahoo.com>
2018-02-04 21:22:02 -0800

    comments are awesome as well

--- js/app.js
+++ js/app.js
@@ -1,41 +1,42 @@
 // Enemies our player must avoid
 "use strict";
 
-function getRandomInt(max, min) {
+function getRandomInt(max, min) {  //ramdom number generator, used for the speed of each bug on respawn
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-class Character {
+class Character {  // Defines the X and Y coordinate values of anything of character class
   constructor(ex, why, speed) {
     this.x = ex;
     this.y = why;
   }
 }
 
-class Enemy extends Character{
+class Enemy extends Character{  // the class enemy is created to represent the bugs
   constructor(ex, why, speed) {
-    super(ex, why, speed);
+    super(ex, why, speed); // fetches the parameters for this class when instantiating enemy class for bug coordinates
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
   }
-  update(dt) {
+  update(dt) { // calculations happening every frame before the game renders that frame for each enemy
     this.x += this.speed * dt;
     if (this.x > 515) {
       this.x = -125;
       this.speed = getRandomInt(600, 150);
     }
   }
-  render() {
+  render() { //renders bug on that frame
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 }
 
+// the three bugs
 let buggy1 = new Enemy(88, 220, getRandomInt(600, 150));
 let buggy2 = new Enemy(88, 137, getRandomInt(600, 150));
 let buggy3 = new Enemy(88, 54, getRandomInt(600, 150));
 
 
-class Player extends Character{
+class Player extends Character{ // the player's class, inheriting x and y properties from character
   constructor() {
     super();
     this.sprite = 'images/char-boy.png';
@@ -53,7 +54,7 @@ class Player extends Character{
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  handleInput(arrowKeys) {
+  handleInput(arrowKeys) {  // defines what happens when the event listener has arrow keys, which direction and if at the edge of the board what to do
     switch(arrowKeys){
       case 'left':
       this.x -= 101;
@@ -84,12 +85,12 @@ class Player extends Character{
   }
 }
 
-let player = new Player() ;
+let player = new Player() ; // the player class instance
 
-let allEnemies = [buggy1, buggy2, buggy3];
+let allEnemies = [buggy1, buggy2, buggy3]; // the array which handles the enemies
 
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function(e) { // event listener to listen for when you click arrow keys
   var allowedKeys = {
     37: 'left',
     38: 'up',
