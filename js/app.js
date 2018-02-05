ff1328de1b6a0e834666406d171c148edc2d26ef
AidzPrehot <aprehatny@yahoo.com>
2018-02-04 17:57:11 -0800

    Added Collision logic to player bug/water, Added handle input functionality to player, added enemy speed randomization

--- js/app.js
+++ js/app.js
@@ -1,81 +1,95 @@
 // Enemies our player must avoid
 
-let bugMovement = [60, 140, 220]
-
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = 88;
-    this.y = 220;
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor(ex, why, speed) {
+    this.x = ex;
+    this.y = why;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x++
-    if (this.x === 515) {
-      this.x = -125;
-    }
-};
-
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-let buggy1 = new Enemy();
-
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-class Player {
-  constructor() {
-    this.sprite = 'images/char-boy.png';
-    this.x = 88;
-    this.y = 220;
   }
-  update() {
-
+  update(dt) {
+  this.x += this.speed * dt
+    if (this.x > 515) {
+      this.x = -125;
+      this.speed = getRandomInt(600, 150)
+    }
   }
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  handleInput() {}
+}
+
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let buggy1 = new Enemy(88, 220, getRandomInt(600, 150));
+let buggy2 = new Enemy(88, 137, getRandomInt(600, 150));
+let buggy3 = new Enemy(88, 54, getRandomInt(600, 150));
+
+
+
+class Player {
+  constructor() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 386;
+  }
+  update() {
+    for (let n = 0; n < 3; n++){
+    if (Math.abs(player.x - allEnemies[n].x) <= 81 && (player.y === allEnemies[n].y)) {
+      this.x = 202;
+      this.y = 386;
+    }
+  }
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput(arrowKeys) {
+    switch(arrowKeys){
+      case 'left':
+      this.x -= 101
+      if (this.x < 0) {
+        this.x = 0;
+      }
+      break;
+      case 'up':
+      this.y -= 83
+      if (this.y < -15) {
+        this.y = 386;
+        this.x = 202;
+      }
+      break;
+      case 'right':
+      this.x += 101
+      if (this.x > 404) {
+        this.x = 404;
+      }
+      break;
+      case 'down':
+      this.y += 83
+      if (this.y > 400) {
+        this.y = 400;
+      }
+      break;
+    }
+  }
 }
 
 let player = new Player() ;
 
-function start(xCord, yCord) {
-player.x = xCord;
-player.y = yCord;
-}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+let allEnemies = [buggy1, buggy2, buggy3];
 
-let allEnemies = [buggy1];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
